6fdddfc82691302d60e6ad984bde8557e4aff93c
ramkumarmu <46369849+ramkumarmu@users.noreply.github.com>
2021-07-14 18:18:14 +0530

    Delete backend.tf

--- 02-Terraform/Hands-on/04_TF_State/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "myFirstResourceGroup"
-    storage_account_name  = "saterraformtraining"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
